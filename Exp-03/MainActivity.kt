501e376e849fa0e3e0c249252617587d98595d8c
mohneesh2001 <87600778+mohneesh2001@users.noreply.github.com>
2024-05-18 12:09:50 +0530

    Add files via upload

---
+++ Exp-03/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.exp_3_sample_shapes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
